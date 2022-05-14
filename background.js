6b056e8dd82477a19525f6de68ee018a31d2a29e
Brat-vseznamus <fvixnin@gmail.com>
2022-05-15 00:48:20 +0200

    v2

---
+++ background.js
@@ -0,0 +1,53 @@
+let color = '#3aa757';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  chrome.storage.sync.set({ "press": false })
+  chrome.storage.sync.set({ "tabs": [] })
+  chrome.storage.sync.set({ "times": {} })
+});
+
+setInterval(async () => {
+    chrome.storage.sync.get(['tabs', 'times'], function(result) {
+        result.tabs.forEach((tabId) => {
+            try {
+                chrome.scripting.executeScript({
+                    target: { tabId: tabId },
+                    function: checkTime,
+                    args: [result.times[tabId.toString()]],
+                });
+            } catch (e) {
+                console.error(e);
+            }
+        })
+    })
+}, 10000)
+
+async function getTabs() {
+    chrome.storage.sync.get(['tabs'], function(result) {
+        console.log('tabs = ' + JSON.stringify(result.tabs))
+    })
+}
+
+
+function checkTime(timeDiff) {
+    try {
+        let innerDoc = document.querySelector("#videoFrame").contentWindow.document
+        let currTimeDiv = "#main-video > div.vjs-control-bar > div.vjs-current-time.vjs-time-controls.vjs-control > div"
+        let fullTimeDiv = "#main-video > div.vjs-control-bar > div.vjs-duration.vjs-time-controls.vjs-control > div"
+
+        let currTimeParts = innerDoc.querySelector(currTimeDiv).innerHTML.split(" ")[3].split(":")
+        let fullTimeParts = innerDoc.querySelector(fullTimeDiv).innerHTML.split(" ")[2].split(":")
+
+        let currTime = parseInt(currTimeParts[0]) * 60 + parseInt(currTimeParts[1])
+        let fullTime = parseInt(fullTimeParts[0]) * 60 + parseInt(fullTimeParts[1])
+
+        // console.log('Time until the end ' + (fullTime - currTime) + 'ms')
+        if ((fullTime - currTime) < timeDiff && fullTime != 0) {
+            let nextButton = document.querySelector("body > div.body-container > div > div > div > div.col.s12.m8.l9.col-even > div:nth-child(3) > div > div.m-select-sibling-episode > a:nth-child(2)")
+            nextButton.click()
+        }
+    } catch (e) {
+        console.error(e)
+    }
+}
---
+++ popup.js
@@ -0,0 +1,99 @@
+let changeColor = document.getElementById("changeColor");
+
+const GREEN = 'rgb(59 165 59)'
+const RED = 'rgb(173 28 28)'
+
+
+const setButtonColor = function(green_) {
+    changeColor.style.backgroundColor = green_
+        ? GREEN
+        : RED;
+};
+
+const setTextColor = function(green_) {
+    changeColor.textContent = green_
+        ? 'enabled'
+        : 'disabled';
+};
+
+const addOrRemoveTabId = function(result, tabId) {
+    var s = result.tabs;
+    var add_ = false
+    if (s.includes(tabId)) {
+        s = s.filter((el) => el != tabId);
+    } else {
+        s.push(tabId);
+        add_ = true
+    }
+    chrome.storage.sync.set({ "tabs": s });
+    return add_
+}
+
+chrome.storage.sync.get(['press', 'tabs', 'times'], async (result) => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    if (tab.id.toString() in result.times) {
+        setButtonColor(result.tabs.includes(tab.id))
+        setTextColor(result.tabs.includes(tab.id))
+    }
+})
+
+changeColor.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.storage.sync.get(['press', 'tabs', 'times'], function(result) {
+
+        console.log(result.times);
+        if (!result.tabs.includes(tab.id) 
+        && !(tab.id.toString() in result.times)) {
+            result.times[tab.id.toString()] = -1
+            chrome.storage.sync.set({times: result.times})
+
+            const i = createInput(result, tab)
+
+            changeColor.parentNode.replaceChild(i, changeColor);
+
+        } else if (result.tabs.includes(tab.id)) {
+            var red = addOrRemoveTabId(result, tab.id);
+
+            setButtonColor(red)
+            setTextColor(red)
+
+        } else {
+            chrome.storage.sync.set({times: result.times})
+
+            const i = createInput(result, tab)
+            i.value = (Math.floor(result.times[tab.id.toString()] / 60)).toString() 
+                + ":" 
+                + (result.times[tab.id.toString()] % 60).toString();
+
+            changeColor.parentNode.replaceChild(i, changeColor);
+
+        }
+    })
+});
+
+function createInput(result, tab) {
+    const i = document.createElement("input");
+    i.setAttribute('type', "text");
+    i.setAttribute('name', "time");
+    i.setAttribute('placeholder', "enter time (xx:xx)")
+    i.style.width = "170px"
+    i.addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+            shut(i.value, tab.id, result)
+            var green_ = addOrRemoveTabId(result, tab.id);
+
+            setButtonColor(green_)
+            setTextColor(green_)
+            i.parentNode.replaceChild(changeColor, i)
+        }
+    })
+    return i;
+}
+
+function shut(text, tabId, result) {
+    console.log("shut " + text);
+    var st = text.split(":")
+    result.times[tabId.toString()] = parseInt(st[0]) * 60 + parseInt(st[1])
+    chrome.storage.sync.set({'times': result.times})
+}
